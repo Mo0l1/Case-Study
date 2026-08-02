3ca7ceaea145751357bfc8d67641749ccdc653c9
Mario <156945358+Mo0l1@users.noreply.github.com>
2026-08-02 14:28:37 +0200

    Update 02_null_and_duplicate_checks.sql

--- sql/02_null_and_duplicate_checks.sql
+++ sql/02_null_and_duplicate_checks.sql
@@ -1,17 +1,53 @@
+-- Step 1: Checking for Null values
 SELECT 
-COUNT(*) AS total_rows,
-COUNTIF(ride_id IS NULL) AS null_ride_id,
-COUNTIF(rideable_type IS NULL) AS null_rideable_type,
-COUNTIF(started_at IS NULL) AS null_started_at,
-COUNTIF(ended_at IS NULL) AS null_ended_at,
-COUNTIF(start_station_name IS NULL) AS null_start_station_name,
-COUNTIF(start_station_id IS NULL) AS null_start_station_id,
-COUNTIF(end_station_name IS NULL) AS null_end_station_name,
-COUNTIF(end_station_id IS NULL) AS null_end_station_id,
-COUNTIF(start_lat is NULL) AS null_start_lat,
-COUNTIF(start_lng is NULL) AS null_start_lng, 
-COUNTIF(end_lat is NULL) AS null_end_lat,
-COUNTIF(end_lng is NULL) AS null_end_lng, 
-COUNTIF(member_casual IS NULL) AS null_member_casual
+      COUNT(*) AS total_rows,
+      COUNTIF(ride_id IS NULL) AS null_ride_id,
+      COUNTIF(rideable_type IS NULL) AS null_rideable_type,
+      COUNTIF(started_at IS NULL) AS null_started_at,
+      COUNTIF(ended_at IS NULL) AS null_ended_at,
+      COUNTIF(start_station_name IS NULL) AS null_start_station_name,
+      COUNTIF(start_station_id IS NULL) AS null_start_station_id,
+      COUNTIF(end_station_name IS NULL) AS null_end_station_name,
+      COUNTIF(end_station_id IS NULL) AS null_end_station_id,
+      COUNTIF(start_lat is NULL) AS null_start_lat,
+      COUNTIF(start_lng is NULL) AS null_start_lng, 
+      COUNTIF(end_lat is NULL) AS null_end_lat,
+      COUNTIF(end_lng is NULL) AS null_end_lng, 
+      COUNTIF(member_casual IS NULL) AS null_member_casual
 FROM 
-`<your_project_name>.cyclist_trip_data.last_12_months` 
+      `<your_project_name>.cyclist_trip_data.last_12_months` 
+
+-- Step 2: Count distinct ride_ids
+
+SELECT 
+      COUNT(DISTINCT ride_id) AS disitinct_ride_id
+FROM 
+      `<your_project_name>.cyclist_trip_data.last_12_months`
+
+-- Step 3: find duplicate rows
+
+SELECT 
+      *
+FROM 
+      `<your_project_name>.cyclist_trip_data.last_12_months`
+WHERE 
+      ride_id IN (
+      SELECT 
+            ride_id
+      FROM 
+            `<your_project_name>.cyclist_trip_data.last_12_months`
+      GROUP BY 
+            ride_id
+      HAVING 
+      COUNT(*) > 1
+      )
+ORDER BY 
+ride_id
+
+-- Step 4: Remove exact duplicates, create cleaned table
+
+CREATE OR REPLACE TABLE `<your_project_name>.cyclist_trip_data.last_12_months_clean` AS
+SELECT DISTINCT 
+      *
+FROM 
+      `<your_project_name>.cyclist_trip_data.last_12_months`

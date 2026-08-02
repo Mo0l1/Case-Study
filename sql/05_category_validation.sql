4cddb8098cf7e26c2db44660b098843b09be05a9
Mario <156945358+Mo0l1@users.noreply.github.com>
2026-08-02 14:49:29 +0200

    Update 05_category_validation.sql

--- sql/05_category_validation.sql
+++ sql/05_category_validation.sql
@@ -1,1 +1,20 @@
+-- Step 1: Check if there are only two different types of bikes
 
+SELECT 
+      rideable_type,
+      COUNT(*) AS total
+FROM 
+      `<your_project_name>.cyclist_trip_data.last_12_months_clean` 
+GROUP BY 
+      rideable_type
+
+
+-- Step 2: Check if there are only two types of bike users
+
+SELECT
+      member_casual,
+      COUNT(*) AS total
+FROM
+      `<your_project_name>.cyclist_trip_data.last_12_months_clean`
+GROUP BY
+      member_casual
